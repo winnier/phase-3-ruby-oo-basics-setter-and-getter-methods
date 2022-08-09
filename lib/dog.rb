738771138d86fe76b7b28696f696ee04dd563691
Winnie Mendoza <wrm956@gmail.com>
2022-08-09 09:15:13 -0400

    commit tree 60045b903c40bdbbe000a6d0b30df06a21ded612

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,19 @@
 class Dog
+    # attr_accessor :name, :breed
+  def name=(name)
+    @name = name
+  end 
+  
+  def name
+    @name
+  end
+
+  def breed=(breed)
+    @breed = breed
+  end
+
+  def breed
+    @breed
+  end
 
 end
